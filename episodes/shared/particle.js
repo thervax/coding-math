8b4f0775df63447fc5c00f3f8430cfcca5e952b4
thervax <thervax@users.noreply.github.com>
2026-03-18 22:48:41 +0200

    episode 8

---
+++ episodes/shared/particle.js
@@ -0,0 +1,12 @@
+class Particle {
+  constructor(x, y, speed, direction) {
+    this.position = new Vector(x, y);
+    this.velocity = new Vector(0, 0);
+    this.velocity.setLength(speed);
+    this.velocity.setAngle(direction);
+  }
+
+  update() {
+    this.position.addTo(this.velocity);
+  }
+}
